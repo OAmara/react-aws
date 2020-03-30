76c786ddca1038e3f5d831e2985e78ce77cee752
Omar Amara <O_amara@hotmail.com>
2020-03-29 23:05:12 -0500

    Router setup, installed styled-components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { } from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
+import Routes from './Routes'
 
 function App() {
   // Router will start in app or will live in route container for seperation of concerns.
   return (
     <div className="App">
-    <h3>Login/Register</h3>
-    {/*
-      <h1>Hello. Start of login with router and hooks.</h1>
-      <h2>Set-up form with state to process body content for fetch response to login & register</h2>
-    */}
+      <h3>Login/Register</h3>
+      {/*
+        <h1>Hello. Start of login with router and hooks.</h1>
+        <h2>Set-up form with state to process body content for fetch response to login & register</h2>
+      */}
+      <Router>
+        <Routes />
+      </Router>
     </div>
   );
 }
---
+++ src/Routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Switch, Route, Link, Redirect } from 'react-router-dom'
+
+// Component that is source for user routes/ pages in app
+export default function Routes(props) {
+	return(
+		<h1>Route here</h1>
+		{
+			// create seperate User login components here and route then redirect upon login/ register 'success'.
+
+			// ^In components, create seperate forms using styled components
+		}
+	)
+}
